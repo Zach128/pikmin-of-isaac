c2beccb562dec734f645560042a918e49a03296d
Zachary Cauchi <zach.cauchi@hotmail.com>
2020-08-29 23:39:14 +0200

    feat: add script to copy to local install dir

---
+++ scripts/ModCopyLocally/CopyToDir.ps1
@@ -0,0 +1,3 @@
+rmdir "~/Documents/My Games/Binding of Isaac Afterbirth+ Mods/pikmin of isaac" -Recurse
+mkdir "~/Documents/My Games/Binding of Isaac Afterbirth+ Mods/pikmin of isaac"
+copy -Path $PSScriptRoot+"/../../src/*" -Destination "~/Documents/My Games/Binding of Isaac Afterbirth+ Mods/pikmin of isaac" -Recurse
