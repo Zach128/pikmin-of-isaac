7c2eeb2ca26feb091ed1e7fd729dc79996b1a95e
Zachary Cauchi <zach.cauchi@hotmail.com>
2020-09-04 08:50:54 +0200

    feat: add pik seed pickups

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,7 @@
 local Mod = RegisterMod("Pikmin of Isaac", 1)
 
 Pik = require("pik")
+PikPickup = require("pik_pickup")
 
 Pik:InjectCallbacks(Mod)
+PikPickup:InjectCallbacks(Mod)
---
+++ src/pik_pickup.lua
@@ -0,0 +1,111 @@
+local game = Game()
+local sound = SFXManager()
+
+local PikPickup = {}
+
+helpers = require("helpers")
+
+PickupVariant.PICKUP_PIK = Isaac.GetEntityVariantByName("Blue Pik Seed")
+PikSubType = {
+    PIK_BLUE = 1
+}
+
+-- Screen coordinates for Pik UI
+PikUILayout = {
+    PIK_FRAME = 12,
+    PIK_ICON = Vector(28, 32),
+    PIK_NUM = Vector(42, 36)
+}
+
+local MAX_PIKS = 99
+
+-- Fetch necessary graphics
+local HudPickups = Sprite()
+HudPickups:Load("gfx/ui/hudpickups.anm2", true)
+local HudNumbers = Sprite()
+HudNumbers:Load("font/counter_numbers.anm2", true)
+
+PikPickup.Mod = nil
+
+-- Render a 2-digit number.
+function RenderNumber(n, Position)
+    if n == nil then n = 0 end
+    -- Second digit
+    HudNumbers:SetFrame("Idle", n//10)
+    HudNumbers:RenderLayer(0, Position)
+    -- First digit
+    HudNumbers:SetFrame("Idle", n % 10)
+    HudNumbers:RenderLayer(0, Position + Vector(6,0))
+end
+
+-- Save the current state accordingly.
+function PikPickup:SaveState()
+    local player = Isaac.GetPlayer(0)
+    local SaveData = ""
+    -- Pad with 0 if number is single-digit
+    if player:GetData().Piks < 10 then
+        SaveData = SaveData .. "0"
+    end
+    SaveData = SaveData .. player:GetData().Piks
+    Isaac.DebugString("Piks Save Data: " .. SaveData)
+    PikPickup.Mod:SaveData(SaveData)
+end
+
+function PikPickup:onUpdate(player)
+    -- Give the player an initial batch of piks.
+    if game:GetFrameCount() == 1 then
+        player:GetData().Piks = 20
+    elseif player.FrameCount == 1 and PikPickup.Mod:HasData() then
+        local ModData = PikPickup.Mod:LoadData()
+        -- Load the first two characters as the number of piks the player has.
+        player:GetData().Piks = tonumber(ModData:sub(1,2))
+    end
+
+    -- Loop through all entities currently on-screen.
+    for _, entity in pairs(Isaac.GetRoomEntities()) do
+        -- Check if the player is touching the pickup
+        if entity.Type == EntityType.ENTITY_PICKUP
+        and (player.Position - entity.Position):Length() < player.Size + entity.Size
+        then
+            --onPickup (userdata cannot be sent to lua functions)
+            if entity.Variant == PickupVariant.PICKUP_PIK
+            and entity:GetSprite():IsPlaying("Idle")
+            and entity:GetData().Picked == nil then
+                -- Pick up a Pik
+                entity:GetData().Picked = true
+                entity.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
+                entity:GetSprite():Play("Collect", true)
+                sound:Play(SoundEffect.SOUND_PLOP, 1, 0, false, 1)
+                if entity.SubType == PikSubType.PIK_BLUE then
+                    player:GetData().Piks = math.min(player:GetData().Piks + 1, MAX_PIKS)
+                end
+
+                -- Save state afterwards in case the player exits
+                PikPickup:SaveState()
+            end
+        end
+
+        -- If the entity has been picked up, delete it.
+        if entity.Type == EntityType.ENTITY_PICKUP
+        and entity:GetData().Picked == true
+        and entity:GetSprite():GetFrame() == 6
+        then
+            entity:Remove()
+        end
+    end
+end
+
+function PikPickup:onRender()
+    local player = Isaac.GetPlayer(0)
+    HudPickups:SetFrame("Idle", PikUILayout.PIK_FRAME)
+    HudPickups:RenderLayer(0, PikUILayout.PIK_ICON)
+    RenderNumber(player:GetData().Piks, PikUILayout.PIK_NUM)
+end
+
+function PikPickup:InjectCallbacks(Mod)
+    PikPickup.Mod = Mod
+    Mod:AddCallback(ModCallbacks.MC_POST_RENDER, PikPickup.onRender)
+    Mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, PikPickup.onUpdate)
+end
+
+return PikPickup
