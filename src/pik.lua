80637e64cb2fb5851f4d260169ffddcdcd4b329c
Zachary Cauchi <zach.cauchi@hotmail.com>
2020-10-01 00:40:24 +0200

    fix: cache-checking leaves phantom piks when pikCount == 0

--- src/pik.lua
+++ src/pik.lua
@@ -321,10 +321,55 @@ function Pik:onCache(player, cacheFlag)
     -- Perform cache-related checks here such as ensuring expected on-screen pik count is met.
     if cacheFlag == CacheFlag.CACHE_FAMILIARS then
         -- If the player data was initialised, check that the appropriate piks are present.
-        if player:GetData().Piks ~= nil then
-            player:CheckFamiliar(FamiliarVariant.PIK, player:GetData().Piks, RNG())
+        local pikCount = player:GetData().Piks
+        
+        if pikCount ~= nil then
+            local allPiks = Pik:GetAllPiks()
+
+            -- CheckFamiliars does not properly handle our use case, leaving one "phantom" pik, even when all piks should be dead.
+            -- So we must create our own "CheckFamiliar" routine.
+            if #allPiks > pikCount then
+                -- Remove excess piks from the room
+                for i = 1, #allPiks - pikCount, 1
+                do
+                    allPiks[i]:Kill()
+                end
+            elseif #allPiks < pikCount then
+                -- Spawn as many piks on the player as needed to meet the expected amount.
+                for i = 1, pikCount - #allPiks, 1
+                do
+                    Isaac.Spawn(
+                        EntityType.ENTITY_FAMILIAR,
+                        FamiliarVariant.PIK,
+                        0,
+                        player.Position,
+                        Vector(0, 0),
+                        player
+                    )
+                end
+            end
+
+            -- player:CheckFamiliar(FamiliarVariant.PIK, player:GetData().Piks - 1, RNG())
         end
     end
+end
+
+function Pik:GetAllPiks()
+    -- Get a list of all piks in the room.
+
+    local allEntities = Isaac.GetRoomEntities()
+    local pikEntities = {}
+
+    for i,entity in pairs(allEntities)
+    do
+        if entity.Type == EntityType.ENTITY_FAMILIAR
+        and entity.Variant == FamiliarVariant.PIK
+        then
+            table.insert(pikEntities, entity)
+        end
+    end
+
+    return pikEntities
 end
 
 function Pik:GetRoomCollideablePiks()
